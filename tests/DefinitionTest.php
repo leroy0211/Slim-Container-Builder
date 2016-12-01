f00d508501203e015ce77a90f50741d612a981bf
Leroy <leroybaeyens@gmail.com>
2016-12-01 21:40:26 +0100

    Changed unittest

--- tests/DefinitionTest.php
+++ tests/DefinitionTest.php
@@ -2,7 +2,6 @@
 
 namespace Flexsounds\Slim\ContainerBuilder\Tests;
 
-
 use Flexsounds\Slim\ContainerBuilder\Definition;
 
 class DefinitionTest extends \PHPUnit_Framework_TestCase
@@ -10,22 +9,23 @@ class DefinitionTest extends \PHPUnit_Framework_TestCase
 
     public function testCreateDefinition()
     {
-        try{
-            Definition::createDefinition();
-            $this->fail("Cannot create a definition without options");
-        }catch (\Exception $e){
-            // Exception thrown, so all is OK!
-        }
+        $definition = Definition::createDefinition(array());
+        $this->assertEquals(null, $definition->getClass());
     }
 
     public function testCreateInstance()
     {
         $definition = new Definition();
-
-        $definition->setClass('\Location\To\A\Class');
-
+        $this->assertEquals($definition, $definition->setClass('\Location\To\A\Class'));
         $this->assertEquals('\Location\To\A\Class', $definition->getClass());
-    }
 
+        $this->assertEquals($definition, $definition->setArguments(array('John' => 'Doe')));
+        $this->assertEquals(array(
+            'John' => 'Doe'
+        ), $definition->getArguments());
+
+        $this->assertEquals($definition, $definition->setFactory('1'));
+        $this->assertEquals(true, $definition->isFactory());
+    }
 
 }
